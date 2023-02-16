39f6a87c0e34b84761e55e791051a81d958d8401
tsxepo <tsxepo@gmail.com>
2023-02-16 15:18:06 +0200

    add cors

--- Web/Program.cs
+++ Web/Program.cs
@@ -17,6 +17,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var MyAllowedSpecificOrigins = "_myAllowedSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowedSpecificOrigins, 
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:5058/api/Ip")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -34,6 +45,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors("MyAllowedSpecificOrigins");
 
 app.MapControllers();
 
