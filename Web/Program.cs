6fa0f5492997bf0838b6c9e4c4616636a8750ddd
tsxepo <tsxepo@gmail.com>
2023-02-16 22:18:34 +0200

    Modify cors

--- Web/Program.cs
+++ Web/Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseCors("MyAllowedSpecificOrigins");
+app.UseCors(MyAllowedSpecificOrigins);
 
 app.MapControllers();
 
